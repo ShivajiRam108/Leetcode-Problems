206bd960429a1a6e241064b0d140b085d320c186
ShivajiRam108-github <ramudus684@gmail.com>
2025-05-17 18:07:47 +0530

    Difficulty

---
+++ CodeChef/1 to 500 Problems/234.py
@@ -0,0 +1,25 @@
+'''
+Double Rent
+------------------
+-> Chefina decided to move into Chef's apartment.Chef was initially paying a rent of X rupees. 
+-> Since Chefina is moving in, the owner decided to double the rent.
+-> Find the final rent Chef needs to pay.
+
+Input Format
+--------------
+-> The input consists of a single integer X, denoting the rent Chef was initially paying.
+
+Output Format
+---------------
+-> Output on a new line, the final rent Chef needs to pay.
+
+sample :-
+input                   output
+2                           4
+3                           6
+10                          20
+'''
+
+# Answer 
+n = int(input())
+print(n * 2)
