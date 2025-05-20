056aca1cdb42755cb46778304912a61756bedd9e
ShivajiRam108-github <ramudus684@gmail.com>
2025-05-20 16:33:16 +0530

    difficult

---
+++ CodeChef/500 to 1000 problems/669.py
@@ -0,0 +1,32 @@
+'''
+Finding Square Roots
+-------------------------
+-> In olden days finding square roots seemed to be difficult but nowadays it can be easily done using in-built functions available across many languages .
+-> Assume that you happen to hear the above words and you want to give a try in finding the square root of any given integer using in-built functions.
+-> So here's your chance.
+
+Input
+-----------
+-> The first line of the input contains an integer T, the number of test cases.
+-> T lines follow. Each line contains an integer N whose square root needs to be computed.
+
+Output
+----------
+-> For each line of the input, output the square root of the input integer, rounded down to the nearest integer, in a new line.
+
+sample :-
+
+input               output 
+
+3
+10                    3
+5                     2
+10000                 100
+'''
+
+# Answer :-
+import math 
+n = int(input())
+for i in range(n):
+    a = int(input())
+    print(int(math.sqrt(a)))
