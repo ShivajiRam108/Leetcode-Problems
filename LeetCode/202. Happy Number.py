8356df47fe04b922182763a809fb8046feedf66d
ShivajiRam108-github <ramudus684@gmail.com>
2025-05-22 18:02:07 +0530

    happyNumberLogic

--- LeetCode/202. Happy Number.py
+++ LeetCode/202. Happy Number.py
@@ -9,3 +9,9 @@ class Solution:
         return False
 x = Solution()
 print(x.isHappy(13))
+
+'''
+Happy Number Definition: A happy number is a number that eventually reaches 1 when repeatedly replacing it
+by the sum of the squares of its digits. For instance, 19 is a happy number because
+1^2 + 9^2 = 82, 8^2 + 2^2 = 68, 6^2 + 8^2 = 100, and 1^2 + 0^2 + 0^2 = 1.
+'''
