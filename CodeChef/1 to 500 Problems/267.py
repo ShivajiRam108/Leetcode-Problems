61fa903e48e7ed1e99c9ac0adba014210eaa4920
ShivajiRam108-github <ramudus684@gmail.com>
2025-05-24 16:06:27 +0530

    Difficulty

--- CodeChef/1 to 500 Problems/267.py
+++ CodeChef/1 to 500 Problems/267.py
@@ -1,38 +1,35 @@
 '''
-Determine the Score
--------------------------
--> Chef appeared for a placement test.There is a problem worth X points.
--> Chef finds out that the problem has exactly 10 test cases. 
--> It is known that each test case is worth the same number of points.
--> Chef passes N test cases among them. Determine the score Chef will get.
+404 Not Found
+----------------
+-> Chef's website has a specific response mechanism based on the HTTP status code received:
 
-NOTE: See sample explanation for more clarity.
+-> If the response code is 404, the website will return NOT FOUND.
+-> For any other response code different from 404, the website will return FOUND.
+-> Given the response code as X, determine the website response.
 
 Input Format
 --------------
--> First line will contain T, number of test cases. Then the test cases follow.
--> Each test case contains of a single line of input, two integers X and 
--> N, the total points for the problem and the number of test cases which pass for Chef's solution.
+-> The first and only line of input contains a response code X.
 
 Output Format
 --------------
--> For each test case, output the points scored by Chef.
+-> Output on a new line NOT FOUND, if the response code is 404. Otherwise print FOUND.
+-> You may print each character of the string in uppercase or lowercase (for example, the strings FOUND, fouND, FouND, and found will all be treated as identical).
 
-sample:-
 
-input                   output
+sample :-
 
-4
-10 3                      3
-100 10                    100
-130 4                     52
-70 0                      0
+input               output
+
+200                 found
+404                 not found
+301                 found
 '''
 
 # Answer:-
-
 n = int(input())
-for i in range(n):
-    a , b = map(int,input().split())
-    res = (a // 10 ) * b 
-    print(res)
+if n == 404:
+    print("NOT FOUND")
+else:
+    print("FOUND")
+    
