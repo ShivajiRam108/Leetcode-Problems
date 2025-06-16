1e7786d65a10ddc2ecd00955174c9352de3f90a5
ShivajiRam108-github <ramudus684@gmail.com>
2025-06-16 15:50:52 +0530

    updated

--- LeetCode/69. Sqrt(xx).py
+++ LeetCode/69. Sqrt(xx).py
@@ -3,4 +3,12 @@ class Solution:
         res = (x ** 0.5)
         n = int(res)
         return n
-    print(mySqrt(4))
+    
+    #  (Or)
+        return int(x ** 0.5)
+    #  (Or)
+        res = math.sqrt(x)
+        return int(res)
+    # (Or)
+        res = pow(x, 0.5)
+        return int(res)
