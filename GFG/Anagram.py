f414d8dd4d4dfcc0440e41c1ff9a398e791f1f70
ShivajiRam108-github <ramudus684@gmail.com>
2025-06-29 19:56:40 +0530

    Easy

--- GFG/Anagram.py
+++ GFG/Anagram.py
@@ -1,5 +1,21 @@
 class Solution:
     def areAnagrams(self, s1, s2):
        return (sorted(s1) == sorted(s2))
+    
+
+x  = Solution()
+print(x.areAnagrams("geeks", "kseeg"))
+
+#  (Or)
+
+class Solution:
+    def areAnagrams(self, s1, s2):
+        a = {}
+        b = {}
+        for i in s1:
+            a[i] = a.get(i,0) + 1
+        for j in s2:
+            b[j] = b.get(j, 0) +1
+        return a == b
 x  = Solution()
 print(x.areAnagrams("geeks", "kseeg"))
