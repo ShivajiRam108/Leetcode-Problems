d49b93aeac9c9842e05faba5b22d0e68b6375cb0
ShivajiRam108-github <ramudus684@gmail.com>
2025-07-09 21:17:46 +0530

    Basic

---
+++ GFG/Odd or Even.py
@@ -0,0 +1,9 @@
+class Solution:
+    def isEven(seft, n):
+        if n % 2 == 0:
+            return True 
+        else:
+            return False
+x = Solution()
+print(x.isEven(4))  # Output: True
+print(x.isEven(5))  # Output: False
