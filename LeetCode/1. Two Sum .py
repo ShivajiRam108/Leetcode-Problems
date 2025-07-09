5d2c264991c3468b374433651843db0ed0404d67
ShivajiRam108-github <ramudus684@gmail.com>
2025-07-09 21:06:01 +0530

    Easy

--- LeetCode/1. Two Sum .py
+++ LeetCode/1. Two Sum .py
@@ -13,7 +13,10 @@ class Solution:
         
         return res
     
-    # another best case is two pointers but it is given the wrong indexes
+# another best case is two pointers but it is given the wrong indexes
+
+class Solution:
+    def twoSum(self, nums: list[int], target: int) -> list[int]:
         left = 0 
         right = len(nums) - 1
         while left <= right:
@@ -25,6 +28,18 @@ class Solution:
             else:
                 left += 1
         return [-1, -1]
-    #  brute_force approch 
+
+#  brute_force approch 
+class Solution:
+    def twoSum(self, nums: list[int], target: int) -> list[int]:
+        for i in range(len(nums)):
+            for j in range(i + 1, len(nums)):
+                if nums[i] + nums[j] == target:
+                    return [i, j]
+        return [-1,-1]
+x= Solution()
+print(x.twoSum([2,7,11,15], 9))  # Output: [0, 1]
+print(x.twoSum([3,2,4], 6))       # Output: [1, 2]
+print(x.twoSum([3,3], 6))          # Output: [0, 1]
 
         
