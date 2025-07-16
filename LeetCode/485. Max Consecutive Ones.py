d0d2558ffafafeace8b49437846763121f377bd0
ShivajiRam108-github <ramudus684@gmail.com>
2025-07-16 21:44:45 +0530

    Easy

--- LeetCode/485. Max Consecutive Ones.py
+++ LeetCode/485. Max Consecutive Ones.py
@@ -1,38 +1,29 @@
-class Solution:
-    def findMaxConsecutiveOnes(self, nums: list[int]) -> int:
-         # (or)
-        l, zeros = 0, 0
-        for r, n in enumerate(nums):
-            zeros += n == 0
-            if zeros > 0:
-                zeros -= nums[l] == 0
-                l += 1
-        return r - l +1
+# class Solution:
+#     def findMaxConsecutiveOnes(self, nums: list[int]) -> int:
+#          # (or)
+#         l, zeros = 0, 0
+#         for r, n in enumerate(nums):
+#             zeros += n == 0
+#             if zeros > 0:
+#                 zeros -= nums[l] == 0
+#                 l += 1
+#         return r - l +1
 
         # (Or)
-        return max((len(list(g)) for k, g in groupby(nums) if k == 1), default = 0)
-       
-        # (Or)Taking user input
-        l, zeros = 0, 0
-        max_length = 0  # To store the maximum sequence length
+class Solution:
+    def findMaxConsecutiveOnes(self, n: list[int]) -> int:
+        count = 0 
+        max_count = 0 
+        for i in n:
+            if i == 1:
+                count += 1 
+                max_count = max(max_count, count)
+            else:
+                count = 0
+        return max(max_count , count)
+x = Solution()
+print(x.findMaxConsecutiveOnes([1,1,0,1,1,1]))  # Output: 3
+print(x.findMaxConsecutiveOnes([1,0,1,1,0,1,1,0,1]))  # Output: 2
+print(x.findMaxConsecutiveOnes([0,0,0,0,0]))  # Output: 0
 
-        for r, n in enumerate(nums):
-            zeros += n == 0
-
-            if zeros > 0:
-                zeros -= nums[l] == 0
-                l += 1
-
-            max_length = max(max_length, r - l + 1)
-
-        return max_length
-
-
-# Taking user input
-nums = list(map(int, input("Enter space-separated binary numbers (0s and 1s): ").split()))
-
-solution = Solution()
-result = solution.findMaxConsecutiveOnes(nums)
-
-print("Maximum Consecutive Ones:", result)
 
