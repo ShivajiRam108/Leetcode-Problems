95fa499c0427ac5ed935fc94eb24377cfa852cf0
ShivajiRam108-github <ramudus684@gmail.com>
2025-08-05 22:14:19 +0530

    Easy

--- GFG/Peak element.py
+++ GFG/Peak element.py
@@ -1,17 +1,15 @@
 class Solution:   
-    def peakElement(self,arr):
+    def peakElement(self, arr):
         left = 0 
         right = len(arr) - 1
         while left < right:
-            mid = left + (right - left) // 2
+            mid = left + (right - left ) // 2
             if arr[mid] > arr[mid + 1]:
                 right = mid 
             else:
                 left = mid + 1
         return left 
 x = Solution()
-print(x.peakElement([1, 2, 4, 5, 7, 8, 3]))
-
-
-
-
+print(x.peakElement([1, 2, 4, 5, 7, 8, 3]))  # 5 (value 8 is a peak)
+print(x.peakElement([1, 2, 4, 5, 7, 8, 3]))        # 5 (value 8 is a peak)
+print(x.peakElement([10, 20, 15, 2, 23, 90, 80]))  #  5 (value 8 is a peak)
