704d48a6943c2c595972c5b7749ab1185986dd5c
ShivajiRam108-github <ramudus684@gmail.com>
2025-08-20 19:51:29 +0530

    Easy

---
+++ GFG/Insertion Sort.py
@@ -0,0 +1,15 @@
+class Solution:
+    def insertionSort(self, arr):
+        for i in range(1, len(arr)):
+            key = arr[i]
+            j = i - 1
+            while j >= 0 and key < arr[j]:
+                arr[j+1] = arr[j]
+                j -= 1
+            arr[j + 1 ] = key 
+        return arr 
+x = Solution()
+print(x.insertionSort([12, 11, 13, 5, 6]))  # Output: [5, 6, 11, 12, 13]
+print(x.insertionSort([5, 2, 9, 1, 5, 6]))   # Output: [1, 2, 5, 5, 6, 9]
+print(x.insertionSort([3, 0, 2, 5, -1, 4, 1]))  # Output: [-1, 0, 1, 2, 3, 4, 5]
+print(x.insertionSort([4, 3, 2, 1]))  # Output: [1, 2, 3, 4]
